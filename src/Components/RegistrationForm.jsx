62c821c770ad5732fe5702bef0576978da7a0206
Sayan Dey <sayandeyriku@gmail.com>
2023-10-12 00:53:34 +0530

    initial backend setup

--- src/Components/RegistrationForm.jsx
+++ src/Components/RegistrationForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './RegistrationForm.css';
-import { formService } from "../services/form.services";
+
 
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
@@ -13,8 +13,13 @@ const RegistrationForm = () => {
     screenshot: null,
   });
 
+  const form = document.forms["submit-to-google-sheet"];
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbwimYQaeOFy6KrNRlFB5K4bk1bWw-EOm8w2_RWizIMEu8RxV_1mWXVldw6_Hgzu3YwGFA/exec";
+
   const handleChange = (e) => {
     const { name, value } = e.target;
+
     setFormData({ ...formData, [name]: value });
   };
 
@@ -27,30 +32,88 @@ const RegistrationForm = () => {
     e.preventDefault();
     // Handle form submission here, e.g., send data to server or Google Sheets.
     console.log(formData);
-    await formService(formData);
+    // await formService(formData);
+
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => {
+        console.log("Submitted");
+      })
+      .catch((error) => console.error("Error!", error.message));
   };
 
   return (
     <div className="form-container">
       <h2>Register Here! Double Header Quiz presented by CalQulas!</h2>
       <h3>Knowledge Partner: 5Point Education Centre & Business Unplugged</h3>
-      <form onSubmit={handleSubmit}>
+      <form name="submit-to-google-sheet" encType="multipart/form-data">
         <label>
           Name:
           <input
             type="text"
-            name="name"
+            name="name_one"
             value={formData.name}
             onChange={handleChange}
             required
           />
         </label>
-
+        <label>
+          Name for perticipant two:
+          <input
+            type="text"
+            name="name_two"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Address for perticipant one:
+          <textarea
+            name="address_one"
+            value={formData.address}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Address for perticipant two:
+          <textarea
+            name="address_two"
+            value={formData.address}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Gender of perticipant one:
+          <select
+            name="gender_one"
+            value={formData.gender}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </label>
+        <label>
+          Gender of perticipant two:
+          <select
+            name="gender_two"
+            value={formData.gender}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </label>
         <label>
           Phone Number:
           <input
             type="text"
-            name="phoneNumber"
+            name="phone"
             value={formData.phoneNumber}
             onChange={handleChange}
             required
@@ -65,30 +128,6 @@ const RegistrationForm = () => {
             value={formData.email}
             onChange={handleChange}
           />
-        </label>
-
-        <label>
-          Address:
-          <textarea
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-          />
-        </label>
-
-        <label>
-          Gender:
-          <select
-            name="gender"
-            value={formData.gender}
-            onChange={handleChange}
-            required
-          >
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
         </label>
 
         <label>Quiz(s):</label>
@@ -130,7 +169,11 @@ const RegistrationForm = () => {
         </div>
         <div>
           <h3>Payment Method:</h3>
-          <img className="qr-code-img" src="assets/images/qr.jpeg"></img>
+          <img
+            className="qr-code-img"
+            src="assets/images/qr.jpeg"
+            alt="9051879329@paytm"
+          ></img>
           <span>UPI ID: 9051879329@paytm</span>
           <span>A/c Number- 919051879329 </span>
         </div>
@@ -140,10 +183,11 @@ const RegistrationForm = () => {
             type="file"
             accept=".png, .jpg, .jpeg"
             onChange={handleScreenshotChange}
+            name="payment_data"
           />
         </label>
 
-        <button type="submit" disabled={false}>
+        <button type="submit" disabled={false} onClick={handleSubmit}>
           Submit
         </button>
       </form>
